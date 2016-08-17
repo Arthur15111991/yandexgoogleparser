808ef35e40bb389705efe1d04b71e3746100602d
Arthur15111991 <nihlusart@gmail.com>
2016-08-18 00:16:10 +0400

    step 2

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+define('DEVELOPMENT', true);
+
     $php_version = phpversion();
     if (!version_compare($php_version, '5.3.0')) {
         die("Minimal required PHP version is  5.3.0");
     }
 
-    die('stop');
+    ?>
+
+    <form action="action.php" method="post">
+        <input type="text" name="search_text" value="">
+        <button type="submit"></button>
+    </form>
